e515a17e7f880f3b9f03754286901bdc278986f3
Sri <sreenews123@gmail.com>
2024-10-12 16:04:17 -0400

    docker implementation

---
+++ dockerrun.sh
@@ -0,0 +1,5 @@
+docker run \
+--mount type=bind,source="$(pwd)"/data,target=/data \
+--name problem1-fixedwidth-parser  fixedwidth-parser:latest\
+python parse_fixed_width_file.py \
+--spec_file 'data/spec.json' --fixed_file 'data/fixedwidth_data.txt' --csv_file 'data/output_csv_parsed_problem1.csv'
